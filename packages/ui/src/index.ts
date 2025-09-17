9d3d54924f88316a07c24121fb44199d5f0c9942
TheGrieferKnight <grieferknight@yahoo.com>
2025-09-17 21:09:32 +0200

    feat!: removed the summoner spell overlay and associated files and components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,13 +2,10 @@ export * from "./components/ChampionIcon";
 export * from "./components/ChampionMasteryCard";
 export * from "./components/MatchHistoryItem";
 export * from "./components/SidebarNav";
-export * from "./components/SpellGrid";
-export * from "./components/SpellImage";
 export * from "./components/StatCard";
 export * from "./components/SummonerProfile";
 export * from "./components/Titlebar";
 export * from "./components/WebTitlebar";
 
-export * from "./hooks/useCooldowns";
 export * from "./hooks/useDashboardData";
 export * from "./hooks/useGameData";
